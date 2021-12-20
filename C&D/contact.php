8b0cbaec77c8a228be0d48533344feff9e950ca9
Bekithemba T. Matshazi <74741869+BT-Matshazi@users.noreply.github.com>
2021-12-20 20:10:03 +0200

    Add files via upload

---
+++ C&D/contact.php
@@ -0,0 +1,18 @@
+<?php
+ 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "bekithembamatshazi@gmail.com";
+$subject = "Mail From website Peacej Designs";
+$txt ="Name = ". $name . "\r\n Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+if($email!=NULL)
+{
+    mail($to,$subject,$txt,$headers);
+}
+
+ header("Location:index.html");
+?>
